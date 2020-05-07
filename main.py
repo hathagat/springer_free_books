001f809a0f513ad9e52bacbd986bf07089ca5908
BarreiroLeo <a.leo.barreiro@gmail.com>
2020-05-07 03:52:31 -0300

    Eliminada la implementación de docker. No veo el sentido. Refactor en el el nombre de guardado, ahora se guarda como AUTOR - año - Titulo Las subcarpetas ahora tienen el nombre de la categoría en ingles.

--- main.py
+++ main.py
@@ -10,62 +10,64 @@ folder = os.getcwd() + '/downloads/'
 
 if not os.path.exists(folder):
     os.mkdir(folder)
-    
+
 if not os.path.exists("table.xlsx"):
-    books = pd.read_excel('https://resource-cms.springernature.com/springer-cms/rest/v1/content/17863240/data/v2')
+    books = pd.read_excel('https://resource-cms.springernature.com/springer-cms/rest/v1/content/17856246/data/v3')
+    # books = pd.read_excel('https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v5')
+    # books = pd.read_excel('https://resource-cms.springernature.com/springer-cms/rest/v1/content/17863240/data/v2')
 
     # save table:
     books.to_excel(folder + 'table.xlsx')
 else:
-    books = pd.read_excel('table.xlsx', index_col=None, header=None)  
+    books = pd.read_excel('table.xlsx', index_col=None, header=None)
 
 # debug:
 # books = books.head()
 
 print('Download started.')
 
-for url, title, author, pk_name in tqdm(books[['OpenURL', 'Book Title', 'Author', 'German Package Name']].values):
-
+for url, title, author, pk_name, edition in tqdm(books[['OpenURL', 'Book Title', 'Author', 'English Package Name', 'Edition']].values):
     new_folder = folder + pk_name + '/'
 
     if not os.path.exists(new_folder):
         os.mkdir(new_folder)
 
-    r = requests.get(url) 
+    r = requests.get(url)
     new_url = r.url
-
-    new_url = new_url.replace('/book/','/content/pdf/')
-
-    new_url = new_url.replace('%2F','/')
+    new_url = new_url.replace('/book/', '/content/pdf/')
+    new_url = new_url.replace('%2F', '/')
     new_url = new_url + '.pdf'
 
-    final = new_url.split('/')[-1]
-    final = title.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + author.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + final
-    output_file = new_folder+final
+    # final = new_url.split('/')[-1]
+    final = title.replace(',', '-').replace('.', '').replace('/', ' ').replace(':', ' ') + '.pdf'
+    final = edition.replace(',', '-').replace('.', '').replace('/', ' ').replace(':', ' ')[-4:] + ' - '  + final
+    final = author.replace(',', '-').replace('.', '').replace('/', ' ').replace(':', ' ').upper() + ' - ' + final
+    
+    output_file = new_folder + final
     if not os.path.exists(output_file):
         myfile = requests.get(new_url, allow_redirects=True)
         try:
             open(output_file, 'wb').write(myfile.content)
-        except OSError: 
+        except OSError:
             print("Error: PDF filename is appears incorrect.")
-        
-        #download epub version too if exists
-        new_url = r.url
 
-        new_url = new_url.replace('/book/','/download/epub/')
-        new_url = new_url.replace('%2F','/')
+        # download epub version too if exists
+        new_url = r.url
+        new_url = new_url.replace('/book/', '/download/epub/')
+        new_url = new_url.replace('%2F', '/')
         new_url = new_url + '.epub'
 
-        final = new_url.split('/')[-1]
-        final = title.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + author.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + final
-        output_file = new_folder+final
-        
+        final = title.replace(',', '-').replace('.', '').replace('/', ' ').replace(':', ' ') + '.epub'
+        final = edition.replace(',', '-').replace('.', '').replace('/', ' ').replace(':', ' ')[-4:] + ' - '  + final
+        final = author.replace(',', '-').replace('.', '').replace('/', ' ').replace(':', ' ').upper() + ' - ' + final
+        output_file = new_folder + final
+
         request = requests.get(new_url)
         if request.status_code == 200:
             myfile = requests.get(new_url, allow_redirects=True)
         try:
             open(output_file, 'wb').write(myfile.content)
-        except OSError: 
+        except OSError:
             print("Error: EPUB filename is appears incorrect.")
-            
+
 print('Download finished.')
